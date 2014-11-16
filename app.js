7bd0df8aa4eb0aa604bbc6ea0ff08c88a09491d8
ian <ithunt0@gmail.com>
2014-11-15 19:50:13 -0500

    ui router and individual patterns pages

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
-angular.module('meanPatterns', [])
+angular.module('meanPatterns', ['ui.router'])
+    .config([  //'home' state
+        '$stateProvider',
+        '$urlRouterProvider',
+        function($stateProvider, $urlRouterProvider) {
+
+            $stateProvider
+                .state('home', {
+                    url: '/home',
+                    templateUrl: '/home.html',
+                    controller: 'MainCtrl'
+                })
+                .state('patterns', {
+                    url: '/patterns/{id}',
+                    templateUrl: '/patterns.html',
+                    controller: 'PatternsCtrl'
+                });
+
+            //if unspecified route requested, use home
+            $urlRouterProvider.otherwise('home');
+
+        }
+
+    ])
     .factory('patterns', [function() {
         var p = {
 
             patterns : [
-                {name: 'Trey Anastasio', upvotes: 7},
-                {name: 'Page McConnel', upvotes: 5},
-                {name: 'Mike Gordon', upvotes: 4},
-                {name: 'Jon Fishman', upvotes: 3}
+                {name: 'Trey Anastasio', upvotes: 7, comments: [
+                    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+                    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+                ]
+                },
+                {name: 'Page McConnel', upvotes: 5, comments: []},
+                {name: 'Mike Gordon', upvotes: 4 , comments: []},
+                {name: 'Jon Fishman', upvotes: 3, comments: []}
             ]
         }
         return p;
@@ -18,7 +45,10 @@ angular.module('meanPatterns', [])
 
             $scope.addPattern = function() {
                 if($scope.newPatternName && !($scope.newPatternName === ''))  {
-                    $scope.patterns.push({name: $scope.newPatternName, upvotes: 0 });
+                    $scope.patterns.push(
+                        {name: $scope.newPatternName,
+                            upvotes: 0,
+                            comments: []});
                     $scope.newPatternName = '';
                 }
             }
@@ -27,6 +57,26 @@ angular.module('meanPatterns', [])
                 pattern.upvotes += 1;
             };
         }
+
     ])
+    .controller('PatternsCtrl',[
+        '$scope',
+        '$stateParams',
+        'patterns',
+        function($scope, $stateParams, patterns) {
+            $scope.pattern = patterns.patterns[$stateParams.id];
+
+            $scope.addComment = function() {
+                if($scope.body === '') {return;}
+                $scope.pattern.comments.push({
+                    body: $scope.body,
+                    author: 'user',
+                    upvotes: 0
+                });
+                $scope.body = '';
+            };
+        }
+
+    ]);
 ;
 
