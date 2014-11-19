4380d11f8546ace782f4144cf91f87d5506565e7
ian <ithunt0@gmail.com>
2014-11-18 21:45:21 -0500

    Added created date to comments and patterns

--- mean-patterns/models/Comments.js
+++ mean-patterns/models/Comments.js
@@ -4,7 +4,11 @@ var CommentSchema = new mongoose.Schema({
     body: String,
     author: String,
     upvotes: {type: Number, default: 0},
-    pattern: { type: mongoose.Schema.Types.ObjectId, ref: 'Pattern'}
+    pattern: { type: mongoose.Schema.Types.ObjectId, ref: 'Pattern'},
+    created: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 mongoose.model('Comment', CommentSchema);
--- mean-patterns/models/Patterns.js
+++ mean-patterns/models/Patterns.js
@@ -3,7 +3,11 @@ var mongoose = require('mongoose');
 var PatternSchema = new mongoose.Schema({
     name: String,
     upvotes: {type: Number, default: 0},
-    comments: [{ type: mongoose.Schema.Types.ObjectId, ref:'Comment'}] //array of comment references
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref:'Comment'}], //array of comment references
+    created: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 //cb = callback function
